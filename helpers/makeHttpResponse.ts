02ea4451cb5d4a1c0b80b8cb119ced84534dd986
Himanshu Tariyal <tariyal2000@gmail.com>
2021-01-17 15:22:06 +0530

    first

--- helpers/makeHttpResponse.ts
+++ helpers/makeHttpResponse.ts
@@ -4,12 +4,14 @@ export default ({
   res,
   status,
   body,
+  headers = {},
 }: {
   res: Response;
   status: number;
   body: Object;
+  headers?: Object;
 }) => {
-  return res.status(status).json({
+  return res.status(status).set(headers).send({
     status,
     body,
   });
