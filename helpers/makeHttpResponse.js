5a19d127657cf0d6578eb1cb0971e3ba749c495c
Himanshu Tariyal <tariyal2000@gmail.com>
2021-01-23 17:41:35 +0530

    deploy changes

--- helpers/makeHttpResponse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ res, status, body, headers = {}, }) => {
-    return res.status(status).set(headers).send({
-        status,
-        body,
-    });
-};
